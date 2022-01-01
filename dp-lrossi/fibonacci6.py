ed76b82d4918afac2dc401ee7f172d6f6863b86d
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2022-01-01 01:01:01 -0700

    Adding solution 6 for fibonacci, no np.

---
+++ dp-lrossi/fibonacci6.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(6))
+print(fibonacci(10))
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(4))
+    
